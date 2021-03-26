64803508943257e8042a67614c718094debf4f19
Jorge-Blanco-42 <66803425+Jorge-Blanco-42@users.noreply.github.com>
2021-03-25 21:57:32 -0600

    gettoken listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 import { SignXMLService } from './services/signxml';
-import { CreateXMLService} from './services/createXML'
+import { CreateXMLService } from './services/createXML'
 import { SignXML } from './models/signxml';
-import { CreateXML} from './models/createXML'
+import { CreateXML } from './models/createXML'
 import { UserService } from './services/user';
 import { User } from './models/user';
+import { CertificateService } from './services/certificate';
+import { Certificate } from './models/certificate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [SignXMLService, CreateXMLService, UserService]
+  providers: [SignXMLService, CreateXMLService, UserService, CertificateService]
 })
 export class AppComponent implements OnInit {
   title = 'Facturador';
 
   public signXML: SignXML;
   public createXML: CreateXML;
-  public user : User;
-  
-  constructor(private _signXMLService: SignXMLService, private _createXMLService : CreateXMLService,
-    private _userService : UserService){
-     
-    this.signXML = new SignXML("signXML", "signFE", 
+  public user: User;
+  public certificate: Certificate;
+
+  constructor(private _signXMLService: SignXMLService, private _createXMLService: CreateXMLService,
+    private _userService: UserService, private _certificateService: CertificateService) {
+
+    this.signXML = new SignXML("signXML", "signFE",
       "b337c43a00ec8b0ed9882375d56b270f", "pendiente",
       "1994", "FE");
-    
-    var linea = {"1": {"cantidad":"1","unidadMedida":"Sp","detalle":"Impresora","precioUnitario":"10000","montoTotal":"10000","subtotal":"9900","montoTotalLinea":"9900","montoDescuento":"100","naturalezaDescuento":"Pronto pago"},"2":{"cantidad":"1","unidadMedida":"Unid","detalle":"producto","precioUnitario":"10000","montoTotal":"10000","subtotal":"10000","montoTotalLinea":"11170","impuesto":{"1": {"codigo":"01","tarifa":"11.7","monto":"1170"}}}}
 
-    var lineaStr = JSON.stringify(linea) 
+    var linea = { "1": { "cantidad": "1", "unidadMedida": "Sp", "detalle": "Impresora", "precioUnitario": "10000", "montoTotal": "10000", "subtotal": "9900", "montoTotalLinea": "9900", "montoDescuento": "100", "naturalezaDescuento": "Pronto pago" }, "2": { "cantidad": "1", "unidadMedida": "Unid", "detalle": "producto", "precioUnitario": "10000", "montoTotal": "10000", "subtotal": "10000", "montoTotalLinea": "11170", "impuesto": { "1": { "codigo": "01", "tarifa": "11.7", "monto": "1170" } } } }
+
+    var lineaStr = JSON.stringify(linea)
     this.createXML = new CreateXML("genXML", "gen_xml_fe", "50617061800070232071700100001011522773451107756391",
-    "00100001011522773451", "2018-06-17T12:00:00-06:00", "Walner Borbon", "01", "702320717", "Walner Borbon",
-    "6", "02", "03", "01", "En la jungla", "506", "64206205", "506", "00000000", "walner1borbon@gmail.com", "Walner Borbon",
-    "01", "702320717", "6", "02", "03", "01", "506", "84922891", "506", "00000000", "walner.borbon@hotmail.com", 
-    "01", "0", "01", "CRC", "569.48", "0", "10000", "10000", "0", "10000", "10000", "20000", "100", "19900", "1170", "21070",
-    "Jiji", "Bichota", lineaStr, 'False')
+      "00100001011522773451", "2018-06-17T12:00:00-06:00", "Walner Borbon", "01", "702320717", "Walner Borbon",
+      "6", "02", "03", "01", "En la jungla", "506", "64206205", "506", "00000000", "walner1borbon@gmail.com", "Walner Borbon",
+      "01", "702320717", "6", "02", "03", "01", "506", "84922891", "506", "00000000", "walner.borbon@hotmail.com",
+      "01", "0", "01", "CRC", "569.48", "0", "10000", "10000", "0", "10000", "10000", "20000", "100", "19900", "1170", "21070",
+      "Jiji", "Bichota", lineaStr, 'False')
 
     this.user = new User("users", "users_log_me_in", "jorgeBlanco", "426819357");
+    this.certificate = new Certificate("", "", "", "", "");
   }
 
-  ngOnInit(){
-    this._userService.login(this.user).subscribe(
-    //this._signXMLService.signFEXML(this.signXML).subscribe(
+
+  ngOnInit() {
+    this._certificateService.getCertificate("4").subscribe(
       result => {
-        //alert("Factura electronica firmada(?");
-        console.log("This is the answer: ",<any>result)
-    },
+        console.log("This is the answer: ", <any>result)
+        this.certificate = result;
+        console.log(this.certificate);
+        this.getToken(this.certificate);
+      },
       error => {
         //alert(<any>error);
-        console.log(<any>error) 
-    }
-  );
-  
+        console.log(<any>error)
+      }
+    );
+
+  }
+
+  getToken(certificate: Certificate) {
+    this._certificateService.getToken(certificate).subscribe(
+      result => {
+        console.log("This is the answer: ", <any>result);
+      },
+      error => {
+        console.log(<any>error)
+      }
+    );
   }
 
 }
--- src/app/services/certificate.ts
+++ src/app/services/certificate.ts
@@ -3,19 +3,35 @@ import { Certificate } from '../models/certificate';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Global } from './global'
+import { FormBuilder} from "@angular/forms";
 
 @Injectable()
 export class CertificateService {
     public url: string;
+    public backendUrl: string;
 
     constructor(
         public _http: HttpClient
     ) {
+        this.backendUrl = Global.backendUrl;
         this.url = Global.url;
     }
 
-    getCertificate(idUsuario:string): Observable<any>{
+    getCertificate(id: string): Observable<any> {
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.get(this.backendUrl + 'get-certificate/' + id, { headers: headers });
+    }
 
+    getToken(certificate: Certificate): Observable<any> {
+        let headers = new HttpHeaders().set('Content-Type', 'multipart/form-data');
+        let form = new FormData();
+        form.append("w","token");
+        form.append("r","gettoken");
+        form.append("grant_type","password");
+        form.append("client_id","api-stag");
+        form.append("username",certificate.usuario);
+        form.append("password", certificate.contraseña);
+        return this._http.post(this.url,form);
     }
 
 }
