d0f8e5624fda73f041c89ec39ef92d7a4669424e
Jorge-Blanco-42 <66803425+Jorge-Blanco-42@users.noreply.github.com>
2021-04-08 20:47:37 -0600

    avance encabezado y unión con modelo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CreateFacturaComponent } from './components/create-factura/create-factura.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/create-factura/create-factura.component.ts
+++ src/app/components/create-factura/create-factura.component.ts
@@ -1,17 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioFirmadoXML } from '../../services/firmadoXML';
+import { ServicioClaveXML } from '../../services/claveXML'
+import { ServicioCreacionXML } from '../../services/creacionXML';
+import { FirmadoXML } from '../../models/firmadoXML';
+import { CreacionXML } from '../../models/creacionXML';
+import { ServicioEnvioXML } from '../../services/envioXML';
+import { EnvioXML } from '../../models/envioXML';
+import { ClaveXML } from '../../models/claveXML';
+import {TipoCambio} from '../../models/tipoCambio';
+import {ServicioTipoCambio} from '../../services/tipoCambioXML';
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-create-factura',
   templateUrl: './create-factura.component.html',
-  styleUrls: ['./create-factura.component.css']
+  styleUrls: ['./create-factura.component.css'],
+  providers: [DatePipe]
 })
 export class CreateFacturaComponent implements OnInit {
 
   public isCollapsedEmisorData = true;
+  public moneda = "dólares";
+  public datosXML: CreacionXML;
+  public cambio: TipoCambio;
 
-  constructor() { }
+
+  constructor(public datepipe: DatePipe) {
+    this.datosXML = new CreacionXML("","","","","","","","","","","","","","","","",
+                                    "","","","","","","","","","","","","","","","",
+                                    "","","","","","","","","","","","","","","","",
+                                    "","","");
+    this.cambio = new TipoCambio("","","");
+   }
 
   ngOnInit(): void {
+    this.datosXML.condicion_venta = "Contado";
+    this.datosXML.medio_pago = "Efectivo";
+    var fecha = new Date();
+    let latest_date =this.datepipe.transform(fecha, 'yyyy-MM-dd');
+    console.log(latest_date);
+    this.cambio
+  }
+
+  enviar(form :any): void{
+    console.log(form);
   }
 
 }
