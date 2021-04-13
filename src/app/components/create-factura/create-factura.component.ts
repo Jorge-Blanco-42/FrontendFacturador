733e33e71fc4311b2b01422ed34dd1d2284c8a41
Mary <maf.nino7@gmail.com>
2021-04-12 21:15:29 -0600

    tabla de receptores registrados

--- src/app/components/create-factura/create-factura.component.ts
+++ src/app/components/create-factura/create-factura.component.ts
@@ -16,7 +16,29 @@ import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 import { Linea } from 'src/app/models/linea';
 import { OtroCargo } from 'src/app/models/otroCargo';
+import {MatTableDataSource} from '@angular/material/table';
 
+//inicio mary
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+//fin mary
 
 @Component({
   selector: 'app-create-factura',
@@ -26,6 +48,8 @@ import { OtroCargo } from 'src/app/models/otroCargo';
 })
 export class CreateFacturaComponent implements OnInit {
 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   public isCollapsedEmisorData = true;
   public emisorDeshabilitado = true;
@@ -176,5 +200,10 @@ export class CreateFacturaComponent implements OnInit {
   toggle(){
       this.isCollapsedEmisorData = !this.isCollapsedEmisorData;
       this.emisorDeshabilitado = true;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
