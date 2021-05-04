6c00a28d759b7fb1ca1e406b06d47474493ba860
Jorge-Blanco-42 <66803425+Jorge-Blanco-42@users.noreply.github.com>
2021-05-04 16:28:02 -0600

    avances tabla de resumen de documentos
    tabla puesta pero vacía y sin funcionalidades

--- src/app/components/consultar/consultar.component.ts
+++ src/app/components/consultar/consultar.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+
+
+const facturas = [
+  {
+    fecha: "05/05/2021", nombreComercial: "", numeroConsecutivo:"", 
+    claveDocumento:"",tipoDocumento:""
+  },
+  {
+    fecha: "05/05/2021", nombreComercial: "", numeroConsecutivo:"", 
+    claveDocumento:"",tipoDocumento:""
+  },
+  {
+    fecha: "05/05/2021", nombreComercial: "", numeroConsecutivo:"", 
+    claveDocumento:"",tipoDocumento:""
+  },
+  {
+    fecha: "05/05/2021", nombreComercial: "", numeroConsecutivo:"", 
+    claveDocumento:"",tipoDocumento:""
+  },
+
+]
 
 @Component({
   selector: 'app-consultar',
@@ -7,9 +31,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultarComponent implements OnInit {
 
+  columnasFactura: string[] = ['fecha', 'nombreComercial', 'numeroConsecutivo', 'claveDocumento', 'tipoDocumento', 'notaCredito', 'notaDebito', 'enviarCorreo', 'anular'];
+  datosFacturas = new MatTableDataSource(facturas);
+  private paginator!: MatPaginator;
   constructor() { }
 
+  @ViewChild('documentosPaginator') set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.setDataSourceAttributes();
+  }
+
+  setDataSourceAttributes() {
+    if (this.paginator) {
+      this.datosFacturas.paginator = this.paginator;
+    }
+  }
+
   ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.datosFacturas.filter = filterValue.trim().toLowerCase();
   }
 
 }
