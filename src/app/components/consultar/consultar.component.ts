a6ecfb2ae689d0f50f8bd383d797451d2b15140a
Mary <maf.nino7@gmail.com>
2021-05-06 14:45:48 -0600

    dialog anular parte grafica terminada

--- src/app/components/consultar/consultar.component.ts
+++ src/app/components/consultar/consultar.component.ts
@@ -22,6 +22,16 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  // { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  // { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  // { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  // { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  // { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  // { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  // { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  // { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  // { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  // { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 
@@ -112,6 +122,7 @@ export class ConsultarComponent implements OnInit {
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAnular, {
       width: '80%',
+      height: '70%',
       data: ELEMENT_DATA
     });
 
@@ -149,17 +160,43 @@ export class ConsultarComponent implements OnInit {
 })
 export class DialogAnular implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumnsLineas: string[] = ['Producto', 'Cantidad', 'PrecioUnitario', 'Descuento', 'Impuestos', 'Subtotal', 'Total'];
+  displayedColumnsCargo: string[] = ['TipoDocumento', 'Detalle', 'PorcentajeMonto', 'MontoCargo'];
   datosFactura: MatTableDataSource<PeriodicElement> = new MatTableDataSource(ELEMENT_DATA);
+  datosCargo: MatTableDataSource<PeriodicElement> = new MatTableDataSource(ELEMENT_DATA);
+  private paginatorLineas!: MatPaginator;
+  private paginatorCargos!: MatPaginator;
 
   constructor(
     public dialogRef: MatDialogRef<DialogAnular>) {
   }
 
+  @ViewChild('lineasPaginator') set matPaginatorLineas(mp: MatPaginator) {
+    this.paginatorLineas = mp;
+    this.setPaginatorLineas();
+  }
+
+  @ViewChild('cargosPaginator') set matPaginatorCargos(mp: MatPaginator) {
+    this.paginatorCargos = mp;
+    this.setPaginatorCargos();
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+  setPaginatorLineas() {
+    if (this.paginatorLineas) {
+      this.datosFactura.paginator = this.paginatorLineas;
+    }
+  }
+
+  setPaginatorCargos() {
+    if (this.paginatorCargos) {
+      this.datosCargo.paginator = this.paginatorCargos;
+    }
+  }
+
 
   ngOnInit() {
 
