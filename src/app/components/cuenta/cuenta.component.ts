0bb3a72629d9d634944be4e4bd4b6b94574743ff
Mary <maf.nino7@gmail.com>
2021-05-28 21:58:55 -0600

    Form mantenimiento cuenta - usuario

--- src/app/components/cuenta/cuenta.component.ts
+++ src/app/components/cuenta/cuenta.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 
+export interface Cliente {
+  nombre: string, nombreRazonSocial: string,
+  identificacion: string,
+  provincia: string, canton: string,
+  distrito: string, barrio: string, otras_senas:string,
+  telefono: string, fax: string, correo: string, contrasena: string, 
+  confirmarContrasena: string
+}
+
 @Component({
   selector: 'app-cuenta',
   templateUrl: './cuenta.component.html',
@@ -8,9 +17,28 @@ import {MatTabsModule} from '@angular/material/tabs';
 })
 export class CuentaComponent implements OnInit {
 
-  constructor() { }
+  cliente!: Cliente;
+  valido: boolean = true;
+
+  constructor() { 
+    this.cliente = {
+      nombre: "",
+      nombreRazonSocial: "", identificacion: "",
+      provincia: "", canton: "",
+      distrito: "", barrio: "", otras_senas:"",
+      telefono: "", fax: "", correo: "", contrasena:"", confirmarContrasena: ""
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  guardar(cliente : Cliente){
+    console.log(cliente);
+  }
+
+  validarContrasena(){
+    console.log("pendiente")
   }
 
 }
